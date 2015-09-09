6e8b818b2e25451ad82a98154826a92b49a98194
Adam Weitzman <adamcweitzman@gmail.com>
2015-09-09 15:15:36 -0500

    mvp

--- main.js
+++ main.js
@@ -1,23 +1,89 @@
 
 
-// function selectorChange (listid) {
 
-	$('#A1, #A2, #A3').change(function() {
 
-		if ($('#A1').val() === "x" && $('#A2').val() === "x" && $('#A3').val() === "x") {
-		alert("x wins")
+
+
+
+
+
+
+
+
+
+function xory (xo) {
+
+
+	$('#A1, #A2, #A3, #B1, #B2, #B3, #C1, #C2, #C3').change(function() {
+
+
+
+		square1 = $('#A1');
+		square2 = $('#A2');
+		square3 = $('#A3');
+		square4 = $('#B1');
+		square5 = $('#B2');
+		square6 = $('#B3');
+		square7 = $('#C1');
+		square8 = $('#C2');
+		square9 = $('#C3');
+
+
+		if ($(square4).val() === xo && $(square5).val() === xo && $(square6).val() === xo) {
+			alert(xo + " " + "wins");
+			console.log("row3")
+
+		} else if ($(square7).val() === xo && $(square8).val() === xo && $(square9).val() === xo) {
+			alert(xo + " " + "wins");
+			console.log("row2")
+
+		} else if ($(square1).val() === xo && $(square2).val() === xo && $(square3).val() === xo) {
+		alert(xo + " " + "wins");
+		console.log("row1")
+		
+		} else if ($(square1).val() === xo && $(square4).val() === xo && $(square7).val() === xo) {
+		alert(xo + " " + "wins");
+		console.log("row1")
+
+		} else if ($(square2).val() === xo && $(square5).val() === xo && $(square8).val() === xo) {
+		alert(xo + " " + "wins");
+		console.log("row1")
+
+		} else if ($(square3).val() === xo && $(square6).val() === xo && $(square9).val() === xo) {
+		alert(xo + " " + "wins");
+		console.log("row1")
+
+		} else if ($(square1).val() === xo && $(square5).val() === xo && $(square9).val() === xo) {
+		alert(xo + " " + "wins");
+		console.log("row1")
+
+		} else if ($(square3).val() === xo && $(square5).val() === xo && $(square7).val() === xo) {
+		alert(xo + " " + "wins");
+		console.log("row1")
 		}
-	});
 
+ 	});
+}
 
-// $('.class1, .class2').click(some_function);
+xory("x")
+xory("o")
+// winEvaluate(square1,square2,square3,"o");
+// winEvaluate(square4,square5,square6,"x");
+// winEvaluate(square4,square5,square6,"o");
 
-// }
+// winEvaluate(square)
 
+// 	$(square1, square2, square3).change(function() {
 
-// selectorChange('#A3')
+// 		console.log("work?")
 
+// 		if ($(square1).val() === xo && $(square2).val() === xo && $(square3).val() === xo) {
+// 		alert(xo + "wins");
+// 		}
+// 	});
+// };
 
+// winEvaluate('#A1, #A2 , #A3',"x")
 
 
 // next steps: write if statement for more situations (if three 0's...)
