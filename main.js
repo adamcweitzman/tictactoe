29e3694f5e28c4335b98033ffb2d28af525a9cc6
Adam Weitzman <adamcweitzman@gmail.com>
2015-09-09 13:30:21 -0500

    firstlineworks

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-console.log($('#A1').val());
 
-$('#A3').change(function() {
 
-	if ($('#A1').val() && $('#A2').val() && $('#A3').val() === "x") {
-	alert("x wins")
-	}
-});
+// function selectorChange (listid) {
+
+	$('#A1, #A2, #A3').change(function() {
+
+		if ($('#A1').val() === "x" && $('#A2').val() === "x" && $('#A3').val() === "x") {
+		alert("x wins")
+		}
+	});
+
+
+// $('.class1, .class2').click(some_function);
+
+// }
+
+
+// selectorChange('#A3')
+
+
+
+
+// next steps: write if statement for more situations (if three 0's...)
 
